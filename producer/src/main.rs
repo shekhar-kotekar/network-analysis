35968bbc52d235adce0bf03e2177267b3262d37a
shekhar-kotekar <shekhar.kotekar@gmail.com>
2024-04-01 11:48:52 +0200

    failing packet reader written

--- producer/src/main.rs
+++ producer/src/main.rs
@@ -1,3 +1,45 @@
+use std::thread;
+
+use pnet::datalink;
+use pnet::datalink::Channel::Ethernet;
+use tracing::{error, info};
+
 fn main() {
-    println!("Hello, world!");
+    tracing_subscriber::fmt().with_thread_names(true).init();
+    let interfaces = datalink::interfaces();
+    let mut handles = vec![];
+
+    for interface in interfaces {
+        info!("Creating thread for interface: {:?}", interface.name);
+        let handle = thread::spawn(move || {
+            capture_packets(interface.clone());
+        });
+        handles.push(handle);
+    }
+    info!("number of interfaces: {}", handles.len());
+    for handle in handles {
+        handle.join().unwrap();
+    }
+}
+
+fn capture_packets(interface: datalink::NetworkInterface) {
+    let (_, mut rx) = match datalink::channel(&interface, Default::default()) {
+        Ok(Ethernet(tx, rx)) => (tx, rx),
+        Ok(_) => panic!("Unhandled channel type"),
+        Err(e) => error!(
+            "Failed to create datalink channel for interface {}: {}",
+            interface.name, e
+        ),
+    };
+    info!("Start reading packets...");
+    loop {
+        match rx.next() {
+            Ok(packet) => {
+                info!("Received packet: {:?}", packet);
+            }
+            Err(e) => {
+                info!("Failed to read packet: {}", e);
+            }
+        }
+    }
 }
