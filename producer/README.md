d128f925ee64850cfa3c08818953b4458421bffe
shekhar-kotekar <shekhar.kotekar@gmail.com>
2024-04-01 15:59:48 +0200

    network packet capture is working

---
+++ producer/README.md
@@ -0,0 +1,12 @@
+# Network packet capture and producers
+
+## Steps to run the producer
+```
+cd producer
+
+# build the producer executable
+cargo build --release
+
+# run the producer executable using sudo
+sudo target/release/producer
+```
--- producer/src/main.rs
+++ producer/src/main.rs
@@ -2,31 +2,29 @@ use std::thread;
 
 use pnet::datalink;
 use pnet::datalink::Channel::Ethernet;
-use tracing::{error, info};
+use tracing::info;
 
 fn main() {
     tracing_subscriber::fmt().with_thread_names(true).init();
-    let interfaces = datalink::interfaces();
-    let mut handles = vec![];
+    let network_interface_name = "en0";
+    let interface = datalink::interfaces()
+        .into_iter()
+        .filter(|iface| iface.name == network_interface_name)
+        .next()
+        .expect("Failed to get interface");
 
-    for interface in interfaces {
-        info!("Creating thread for interface: {:?}", interface.name);
-        let handle = thread::spawn(move || {
-            capture_packets(interface.clone());
-        });
-        handles.push(handle);
-    }
-    info!("number of interfaces: {}", handles.len());
-    for handle in handles {
-        handle.join().unwrap();
-    }
+    info!("Capturing packets for '{:?}' interface", interface.name);
+    let handle = thread::spawn(move || {
+        capture_packets(interface.clone());
+    });
+    handle.join().unwrap();
 }
 
 fn capture_packets(interface: datalink::NetworkInterface) {
-    let (_, mut rx) = match datalink::channel(&interface, Default::default()) {
+    let (_, mut rx) = match datalink::channel(&interface, datalink::Config::default()) {
         Ok(Ethernet(tx, rx)) => (tx, rx),
         Ok(_) => panic!("Unhandled channel type"),
-        Err(e) => error!(
+        Err(e) => panic!(
             "Failed to create datalink channel for interface {}: {}",
             interface.name, e
         ),
